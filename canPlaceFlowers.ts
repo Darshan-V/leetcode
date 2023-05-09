f15e1fdf8a7cb0643c2a965f3c06dc0392ab231e
Darshan-V <mr.darshanv@gmail.com>
2023-05-09 13:39:17 +0530

    solved can place flowers

---
+++ canPlaceFlowers.ts
@@ -0,0 +1,23 @@
+// You have a long flowerbed in which some of the plots are planted, and some are not.
+// However, flowers cannot be planted in adjacent plots.
+
+// Given an integer array flowerbed containing 0's and 1's,
+// where 0 means empty and 1 means not empty, and an integer n, return true if n new f
+// lowers can be planted in the flowerbed without violating the no-adjacent-flowers rule and false otherwise.
+
+function canPlaceFlowers(flowerbed: number[], n: number): boolean {
+  for (let i = 0; i < flowerbed.length; i++) {
+    if (
+      flowerbed[i] === 0 &&
+      flowerbed[i - 1] !== 1 &&
+      flowerbed[i + 1] !== 1
+    ) {
+      n--
+      i++
+    }
+  }
+  return n <= 0
+}
+
+const input = [1, 0, 0, 0, 1]
+console.log(canPlaceFlowers(input, 2))
